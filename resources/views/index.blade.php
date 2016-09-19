f40cfb1b5885b154a489df6fb84c62502cff1103
Androniki Grigoraki <androniki@Andronikis-MacBook-Pro.local>
2016-09-19 16:56:22 +0300

    Remove Components List From Front Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,9 +12,9 @@
 @include('partials.about-app')
 
 @if(!$component_groups->isEmpty() || !$ungrouped_components->isEmpty())
-<div class="section-components">
-    @include('partials.components')
-</div>
+{{--<div class="section-components">--}}
+    {{--@include('partials.components')--}}
+{{--</div>--}}
 @endif
 
 @if($display_metrics && $app_graphs)
